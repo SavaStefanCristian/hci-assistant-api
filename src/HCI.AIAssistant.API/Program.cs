ba3bb089c464ed03649f3fd2ab3495473605ef05
SavaStefanCristian <savastefancristian@gmail.com>
2025-12-11 14:38:49 +0200

    Add hardcoded instruction

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -89,5 +89,7 @@ Console.WriteLine(app.Services.GetService<ISecretsService>()?.IoTHubSecrets?.Con
 Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.KeyVaultName);
 Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.SecretsPrefix);
 Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.IoTDeviceName);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.Instruction);
+
 
 app.Run();
